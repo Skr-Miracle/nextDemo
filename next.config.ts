0b7ef2fe9be592ea447781134f6452fdebf039a8
skr <526206383@qq.com>
2024-12-26 15:41:27 +0800

    fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // 类型断言，将对象强制转换为符合 NextConfig['eslint'] 要求的类型
+    ...({
+      ignoreDuringBuilds: false,
+      extends: '@next/eslint-plugin-next',
+      rules: {
+        'react/no-unescaped-entities': 'off',
+      },
+    } as any as NextConfig['eslint']),
+  },
 };
+
 
 export default nextConfig;
